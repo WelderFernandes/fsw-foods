dc3b8703c5892c88ed8801985f3680f0f8d41096
Welder Nascimento Fernandes <welderx3@gmail.com>
2024-04-30 10:04:39 -0300

    chore: add @rocketseat/eslint-config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "./_components/ui/button";
+import { Button } from './_components/ui/button'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 ">
+    <div className="flex min-h-screen flex-col items-center  justify-between p-24">
       <Button variant="default">Click me</Button>
     </div>
-  );
+  )
 }
