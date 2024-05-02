7433b37c7d4836cba484e757897e4f045a3590d8
Welder Nascimento Fernandes <welderx3@gmail.com>
2024-05-02 18:17:51 -0300

    feat: add init promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import CategoryList from './_components/category-list'
 import Header from './_components/header'
 import Search from './_components/search'
@@ -11,6 +12,18 @@ export default function Home() {
       </div>
       <div className="px-5 py-6">
         <CategoryList />
+      </div>
+      <div className="px-5 py-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de descontos"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="( max-width: 768px ) 100vw, 768px"
+          quality={100}
+          priority={true}
+        />
       </div>
     </>
   )
