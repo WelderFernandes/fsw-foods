a1db318a83e04201ae4827295092927dbaff00fe
Welder Nascimento Fernandes <welderx3@gmail.com>
2024-05-06 22:32:31 -0300

    feat: add search restaurants page

---
+++ app/restaurants/_actions.search.ts
@@ -0,0 +1,16 @@
+'use server'
+
+import { db } from '../_lib/prisma'
+
+export async function SearchForRestaurants(search: string) {
+  const restaurants = await db.restaurant.findMany({
+    where: {
+      name: {
+        contains: search,
+        mode: 'insensitive',
+      },
+    },
+    take: 10,
+  })
+  return restaurants
+}
